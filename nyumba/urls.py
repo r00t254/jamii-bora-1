78a8e7dae3759e0d7b282003f40d786dae2fb412
WayneMusungu <waynemuyera17@gmail.com>
2022-06-16 14:20:14 +0300

    Configuring the One-Step Workflow

--- nyumba/urls.py
+++ nyumba/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from django_registration.backends.one_step.views import RegistrationView
 from . import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('',views.index, name='index'),
+    path('accounts/register/', RegistrationView.as_view(success_url='/nyumba'),name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    
     
 ]
 
