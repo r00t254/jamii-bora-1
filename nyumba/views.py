e11d17156c925a4c8ae43b86aa00f225252bcfe7
WayneMusungu <waynemuyera17@gmail.com>
2022-06-17 13:15:22 +0300

    Create a for loop that will display all the neighbor hood that has been created by an authenticated user

--- nyumba/views.py
+++ nyumba/views.py
@@ -13,6 +13,8 @@ def index(request):
     hoods = NeighbourHood.objects.all().order_by('-post_date') 
     return render(request, 'index.html',{"profiles": profiles, "hoods":hoods})
 
+
+@login_required(login_url='/accounts/login/')
 def update_profile(request):
     current_user = request.user
     form = ProfileForm(request.POST, request.FILES)
@@ -27,12 +29,14 @@ def update_profile(request):
     return render(request, 'profile-update.html', {'form': form})
 
 
+@login_required(login_url='/accounts/login/')
 def profile(request,pk):
     user = User.objects.get(pk = pk)
     profiles = Profile.objects.filter(user = user).all()
     current_user = request.user
     return render(request,'profile.html',{"current_user":current_user, "user":user, "profiles":profiles})
 
+@login_required(login_url='/accounts/login/')
 def createhood(request):
     current_user = request.user
     form = HoodForm(request.POST, request.FILES)
