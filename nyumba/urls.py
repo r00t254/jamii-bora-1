3d5f9b56b9c0ce7bcffc1c41be6728190a2cb795
WayneMusungu <waynemuyera17@gmail.com>
2022-06-17 07:21:05 +0300

    Add a registration complete form

--- nyumba/urls.py
+++ nyumba/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('accounts/register/', RegistrationView.as_view(success_url='/nyumba'),name='django_registration_register'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     
