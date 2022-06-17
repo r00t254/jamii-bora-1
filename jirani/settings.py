3333393791aa2ae85c32157e67d83246d97f560d
WayneMusungu <waynemuyera17@gmail.com>
2022-06-17 11:53:21 +0300

    Add navbar and create styling

--- jirani/settings.py
+++ jirani/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 import cloudinary
 import cloudinary.uploader
@@ -128,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
