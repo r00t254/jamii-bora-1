118c1fcce95b5d217de1dd9cb1723c4ba81c4c0a
WayneMusungu <waynemuyera17@gmail.com>
2022-06-17 12:42:37 +0300

    Create an index template

--- nyumba/forms.py
+++ nyumba/forms.py
@@ -1,9 +1,18 @@
+from pyexpat import model
 from django.contrib.auth.models import User
 from django import forms
-from .models import Profile
+from .models import NeighbourHood, Profile
 
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['profile_picture','bio','email','phone_no']
+        
+        
+        
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighbourHood
+        fields = ['name','location','description', 'image']
+        
--- nyumba/views.py
+++ nyumba/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
-from .models import Profile
+from .models import Profile, NeighbourHood
 from django.contrib.auth.models import User
-from .forms import ProfileForm
+from .forms import ProfileForm, HoodForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
-    return render(request, 'index.html')
+    current_user = request.user
+    profiles = Profile.objects.filter(id = current_user.id).all()
+    hoods = NeighbourHood.objects.all().order_by('-post_date') 
+    return render(request, 'index.html',{"profiles": profiles, "hoods":hoods})
 
 def update_profile(request):
     current_user = request.user
@@ -28,7 +31,17 @@ def profile(request,pk):
     current_user = request.user
     return render(request,'profile.html',{"current_user":current_user, "user":user, "profiles":profiles})
 
-
-
+def createhood(request):
+    current_user = request.user
+    form = HoodForm(request.POST, request.FILES)
+    if request.method == 'POST':  
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.user = request.user
+            hood.save()
+            return redirect ('index')
+        else:
+            form = ProfileForm()
+    return render(request,'create-hood.html',{'form':form})
 
 
