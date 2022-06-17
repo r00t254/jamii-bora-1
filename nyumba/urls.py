3d231804a39e404e56edee5a6bc68b2b715c8eb1
WayneMusungu <waynemuyera17@gmail.com>
2022-06-17 11:22:27 +0300

    Adjust the registration complete template and add an update profile url

--- nyumba/urls.py
+++ nyumba/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('profile-update/',views.update_profile, name='update_profile'), 
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     
--- nyumba/views.py
+++ nyumba/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Profile
+from .forms import ProfileForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-
+@login_required(login_url='/accounts/login/')
 def index(request):
     return render(request, 'index.html')
+
+def update_profile(request):
+    current_user = request.user
+    form = ProfileForm(request.POST, request.FILES)
+    if request.method == 'POST':  
+        if form.is_valid():
+            details = form.save(commit=False)
+            details.user = request.user
+            details.save()
+            return redirect ('index')
+        else:
+            form = ProfileForm()
+    return render(request, 'profile-update.html', {'form': form})
